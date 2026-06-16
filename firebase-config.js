f1b7ada1f596c217ef760f19397eed35011e4518
Shruti Awasthi <shrutismac@Shrutis-MacBook-Air.local>
2026-06-16 14:49:42 +0530

    Add Firebase project configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
-  authDomain: "REPLACE_WITH_FIREBASE_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_FIREBASE_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_FIREBASE_APP_ID"
+  apiKey: "AIzaSyDcnoENtb_A7zItupufZzR62zWFVq08iC0",
+  authDomain: "my-login-432a6.firebaseapp.com",
+  projectId: "my-login-432a6",
+  storageBucket: "my-login-432a6.firebasestorage.app",
+  messagingSenderId: "980619325529",
+  appId: "1:980619325529:web:a4f38e6080fada70824148"
 };
 
 export const hasFirebaseConfig = Object.values(firebaseConfig).every(
